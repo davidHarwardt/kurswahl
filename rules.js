5396310e350edf269d185079f38805fb42429731
David Harwardt <david.harwardt@icloud.com>
2023-01-09 12:55:42 +0100

    initial

---
+++ rules.js
@@ -0,0 +1,12 @@
+
+// rule: Deutsch 4 Semester
+@rule("Deutsch 4 Semester")
+function rule() {
+
+}
+
+// rule?: Anzahl einzubringender Pflichtkurse <= 32 (wird nicht geprüft!)
+function rule() {
+
+}
+
